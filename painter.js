660425757ec05aa187e3265a4a63cd7e5738a00d
LXing <31298876+FlyOrBoom@users.noreply.github.com>
2020-03-15 09:00:20 -0700

    correct light theme

--- painter.js
+++ painter.js
@@ -54,15 +54,15 @@ function hueToRgb (p, q, t) {
   return p;
 }
 function hslToRgb ( h , s , l ) {
-    let r, g, b
-    if ( s === 0 ) {
-        r = g = b = l; // achromatic
-    } else {
-        const q = l < 0.5 ? l * (1 + s) : l + s - l * s
-        const p = 2 * l - q
-        r = hueToRgb( p , q , h + 1/3 )
-        g = hueToRgb( p , q , h )
-        b = hueToRgb( p , q , h - 1/3 )
-    }
-    return [r,g,b].map(x=>Math.round(x*255))
+  let r, g, b
+  if ( s === 0 ) {
+      r = g = b = l; // achromatic
+  } else {
+      const q = l < 0.5 ? l * (1 + s) : l + s - l * s
+      const p = 2 * l - q
+      r = hueToRgb( p , q , h + 1/3 )
+      g = hueToRgb( p , q , h )
+      b = hueToRgb( p , q , h - 1/3 )
+  }
+  return [r,g,b].map(x=>Math.round(x*255))
 }
